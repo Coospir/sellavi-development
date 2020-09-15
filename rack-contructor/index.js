35f1704d68bf3eaccf4260aa493e961465991015
Селляви Админ <sellaviadmin@MacBook-Pro-Sellavi.local>
2020-09-15 18:33:23 +0300

    Added some fixes in constructor

--- rack-contructor/index.js
+++ rack-contructor/index.js
@@ -16,27 +16,29 @@ $(function() {
                 if(i === 0) {
                     $(".stand-container").append(`<ul class="shelf shelf-container_` + i + `"></ul>`)
                 }
+
                 console.log('IF: count stands: ', i)
             }
 
+            console.log('Count Shelfs input: ', countShelfs)
             for(let j = 0; j < countShelfs; j++) {
-                $(".shelf shelf-container_" + j).append(`<li class="shelff shelf__` + j + `"></li>`);
+                $(".shelf, .shelf-container_" + j).append(`<li class="shelff shelf__` + j + `"></li>`);
                 console.log('IF: count shelfs: ', j)
             }
 
         } else {
             console.log('We are in else')
-            countStands++;
-            for(let i = 0; i < countStands; i++) {
+            //countStands++;
+            for (let i = 0; i < countStands + 1; i++) {
                 $(".stand-container").append(`<li class="stand stand__` + i + `"></li>`).append(`<ul class="shelf shelf-container_` + i + `"></ul>`)
                 console.log('ELSE: count stands: ', i)
             }
 
-            for(let j = 0; j < countShelfs; j++) {
-                $(".shelf shelf-container_" + j).append(`<li class="shelff shelf__` + j + `"></li>`);
+            for (let j = 0; j < countShelfs; j++) {
+                $(".shelf, .shelf-container_" + j).append(`<li class="shelff shelf__` + j + `"></li>`);
                 console.log('ELSE: count shelfs: ', j)
             }
-
+        }
 
             /*for(let i = 0; i < countStands; i++) {
 
@@ -48,7 +50,7 @@ $(function() {
 
             for(let i = 0; i < countShelfs; i++) {
                 $(".shelf-container").append(`<li class="shelf shelf__` + i + `"></li>`);
-            }*/
+            }*!/
         }
 
 
